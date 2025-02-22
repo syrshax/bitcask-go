c2509d4607cb58975f2dab30598be00b8884d743
syrshax <albervf@hotmail.com>
2025-02-22 15:18:01 +0100

    ammend: rewrite again

--- cmd/main.go
+++ cmd/main.go
@@ -11,5 +11,5 @@ type db struct {
 
 func main() {
 	fmt.Printf(" -- Implementation of Bitcask paper -- ")
-	fmt.Printf("Opening a new instance of db chose whatever option you want: \n 1- Create DB \n 2- Open file \n 3- Exit \n")
+	fmt.Printf("Opening a new instance of db \n Choose whatever option you want: \n 1- Create DB \n 2- Open file \n 3- Exit \n")
 }
