1a70a9f68721b25dca8c9671e6993c7357ed406d
syrshax <selvix728@gmail.com>
2025-06-27 22:52:07 +0200

    feat: adding indexing at startup

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	db "bitcask/internal"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf(" -- Implementation of Bitcask paper -- ")
-	fmt.Printf("Opening a new instance of db \n Choose whatever option you want: \n 1- Create DB \n 2- Open file \n 3- Exit \n")
-
+	fmt.Printf(" -- Implementation of Bitcask paper --\n\n")
+	db, _ := db.Open("database")
+	key := []byte("foo")
+	val := []byte("bar")
+	db.Put(key, val)
+	data, _ := db.Get(key)
+	fmt.Printf("KEY: %v\tVALUE: %v\n", string(key), string(data))
 }
