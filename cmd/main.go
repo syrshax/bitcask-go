829826463604cf05d6009c89ddc42dc3874fb99f
syrshax <selvix728@gmail.com>
2025-06-27 21:26:04 +0200

    feat: put and get methods and created bitcask
    and filebitckas structure

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	db "bitcask/internal"
 )
-
-type Bitcask struct {
-	file     *db.BitcaskFile
-	indexing *db.Indexing
-	lock     *db.LockFile
-}
 
 func main() {
 	fmt.Printf(" -- Implementation of Bitcask paper -- ")
 	fmt.Printf("Opening a new instance of db \n Choose whatever option you want: \n 1- Create DB \n 2- Open file \n 3- Exit \n")
+
 }
